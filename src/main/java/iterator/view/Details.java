417d9c818f02a874716530e76dfe4c0c36920a5d
Andrew Kennedy <andrew.kennedy@cloudsoftcorp.com>
2013-09-13 22:02:47 +0100

    Improved CSS for details view
    - Use cell border for matrix brackets
    - Add colour swatch for each transform when available
    - Show percentage wight based on determinant

--- src/main/java/iterator/view/Details.java
+++ src/main/java/iterator/view/Details.java
@@ -19,6 +19,7 @@ import iterator.Explorer;
 import iterator.model.IFS;
 import iterator.model.Transform;
 
+import java.awt.Color;
 import java.awt.Dimension;
 
 import javax.swing.JTextPane;
@@ -47,11 +48,13 @@ public class Details extends JTextPane {
     private static final String[] CSS_RULES = new String[] {
         "h1 { font-family: Calibri, sans-serif; font-style: bold; font-size: 30px; margin-left: 10px; }",
         "h2 { font-family: Calibri, sans-serif; font-style: bold; font-size: 20px; margin-left: 10px; }",
-        ".id { font-family: Calibri, sans-serif; font-style: bold; font-size: 13px; padding-top: 24px; }",
-        ".matrixr1 { font-family: Cambria, serif; font-style: italic; font-size: 11px; padding: 20px 10px -5px 10px; }",
-        ".matrixr2 { font-family: Cambria, serif; font-style: italic; font-size: 11px; padding: -5px 10px -5px 10px; }",
-        ".brackets { font-family: Cambria, serif; font-weight: 100; font-size: 28px; padding: 14px -5px -5px 0px; }",
-        ".ifs { margin-left: 20px; border: 0px; }",
+        ".id { font-family: Calibri, sans-serif; font-style: bold; font-size: 15px; padding: 5px 0 0 0; margin: 0; }",
+        ".info { font-family: Calibri, sans-serif; font-style: italic; font-size: 12px; padding: 0 0 5px 0; margin: 0; }",
+        ".matrixr1 { font-family: Cambria, serif; font-style: italic; font-size: 12px; padding: 5px 5px 0 -5px; margin: 0; }",
+        ".matrixr2 { font-family: Cambria, serif; font-style: italic; font-size: 12px; padding: 0 5px -5px -5px; margin: 0 0 -5px 0; }",
+        ".bracketl { width: 5px; padding: 0; border-left: 2px solid black; border-top: 2px solid black; border-bottom: 2px solid black; }",
+        ".bracketr { width: 5px; padding: 0; border-right: 2px solid black; border-top: 2px solid black; border-bottom: 2px solid black; }",
+        ".ifs { margin-left: 20px; border: 0; }",
     };
 
     public Details(EventBus bus, Explorer controller) {
@@ -104,21 +107,42 @@ public class Details extends JTextPane {
                 html.append("<table class=\"ifs\" width=\"250px\">");
                 double[] matrix = new double[6];
                 t.getTransform().getMatrix(matrix);
+
+                Color c = Color.WHITE;
+                if (controller.isColour()) {
+                    if (controller.hasPalette()) {
+                        c = controller.getColours().get(i % controller.getPaletteSize());
+                    } else {
+                        c = Color.getHSBColor((float) i / (float) ifs.size(), 0.8f, 0.8f);
+                    }
+                }
+                
                 String data = String.format(
                         "<tr class=\"transform\">" +
-                        "<td class=\"id\" rowspan=\"2\">%02d</td>" +
-                        "<td class=\"brackets\" rowspan=\"2\">[</td>" +
-                        "<td class=\"matrixr1\" align=\"right\">%.3f</td>" +
-                        "<td class=\"matrixr1\" align=\"right\">%.3f</td>" +
-                        "<td class=\"matrixr1\" align=\"right\">%.3f</td>" +
-                        "<td class=\"brackets\" rowspan=\"2\">]</td>" +
+                            "<td class=\"id\">%02d" +
+                            "</td>" +
+                            "<td class=\"bracketl\" rowspan=\"2\">&nbsp;</td>" +
+                            "<td class=\"matrixr1\" align=\"right\">%.3f</td>" +
+                            "<td class=\"matrixr1\" align=\"right\">%.3f</td>" +
+                            "<td class=\"matrixr1\" align=\"right\">%.3f</td>" +
+                            "<td class=\"bracketr\" rowspan=\"2\">&nbsp;</td>" +
                         "</tr>" +
                         "<tr class=\"transform\">" +
-                        "<td class=\"matrixr2\" align=\"right\">%.3f</td>" +
-                        "<td class=\"matrixr2\" align=\"right\">%.3f</td>" +
-                        "<td class=\"matrixr2\" align=\"right\">%.3f</td>" +
-                        "</tr>",
-                        t.getId(), matrix[0], matrix[1], matrix[2], matrix[3], matrix[4], matrix[5]);
+                            "<td class=\"info\">%.1f%%" +
+                                "<div style=\"width: 15px; height: 10px; border: 1px solid %s; " +
+                                "background: #%02x%02x%02x; padding: 0; margin: 0;\">&nbsp;</div>" +
+                            "</td>" +
+                            "<td class=\"matrixr2\" align=\"right\">%.3f</td>" +
+                            "<td class=\"matrixr2\" align=\"right\">%.3f</td>" +
+                            "<td class=\"matrixr2\" align=\"right\">%.3f</td>" +
+                        "</tr>" +
+                        "<tr class=\"space\"><td colspan=\"6\">&nbsp;</td></tr>",
+                        i + 1,
+                        matrix[0], matrix[1], matrix[2],
+                        100d * t.getDeterminant() / ifs.getWeight(),
+                        controller.isColour() ? "black" : "white",
+                        c.getRed(), c.getGreen(), c.getBlue(),
+                        matrix[3], matrix[4], matrix[5]);
                 html.append(data);
                 html.append("</table>");
                 html.append("</td>");
